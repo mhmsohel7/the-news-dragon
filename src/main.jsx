d49cf23fd252c4b0e5a10c411aa858a385cde742
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-10-27 17:59:42 +0600

    custom_router_setup-&-pages_setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routers.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
