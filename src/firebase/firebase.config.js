4377d4f6922e97ccf391d2e3923dce6da2320c0a
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-10-27 17:09:06 +0600

    intial-setup-with-firebase-react-router

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDaMckRm3y9ic151Kq4y6_iOpjRkutPSw",
+  authDomain: "the-dragon-news-484a8.firebaseapp.com",
+  projectId: "the-dragon-news-484a8",
+  storageBucket: "the-dragon-news-484a8.appspot.com",
+  messagingSenderId: "687305360121",
+  appId: "1:687305360121:web:351f0309c880dd570a9828",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
