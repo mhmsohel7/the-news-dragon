e0476c4028912b608a487243834a1310e6bc03cd
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-10-31 19:44:09 +0600

    dataLoad from server and show in LeftNav

--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <h1>This is left side</h1>
+      <h1>All category</h1>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
