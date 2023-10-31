ef63e4c6d94b930c84b8a5122f59e666f63a3b90
mhmsohel7 <mdsohelkhan230@gmail.com>
2023-10-31 23:54:31 +0600

    load news category data from server

--- src/pages/Home/category/Category.jsx
+++ src/pages/Home/category/Category.jsx
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import NewsCard from "../NewsCard/NewsCard";
 
 const Category = () => {
   const { id } = useParams();
+
+  //category wise data from routers loader funtion.
+  const categoryNews = useLoaderData();
+
   return (
     <div>
-      <h2>This is Category {id}</h2>
+      <h2>Total Category news is : {categoryNews.length}</h2>
+      {categoryNews.map((news) => (
+        <NewsCard key={news._id} news={news}></NewsCard>
+      ))}
     </div>
   );
 };
--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const News = () => {
   return (
     <div>
-      <h2>this is news</h2>
+      <h2>news details coming soon</h2>
     </div>
   );
 };
